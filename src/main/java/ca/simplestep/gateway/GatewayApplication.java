1ed81c5e643801464879e64c58917ecd21dbe73d
Stephen Doxsee <sdoxsee@gmail.com>
2019-12-06 12:39:05 -0500

    Add frontend-maven-plugin and workaround for root path index

--- src/main/java/ca/simplestep/gateway/GatewayApplication.java
+++ src/main/java/ca/simplestep/gateway/GatewayApplication.java
@@ -2,6 +2,11 @@ package ca.simplestep.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +15,17 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+}
+
+// https://github.com/spring-projects/spring-boot/issues/9785
+@Component
+class CustomWebFilter implements WebFilter {
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+		if (exchange.getRequest().getURI().getPath().equals("/")) {
+			return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build()).build());
+		}
+
+		return chain.filter(exchange);
+	}
 }
